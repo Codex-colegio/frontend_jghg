8ea47dc94d517d51c999a99d8387a4994ea2f8c1
Leonardo-b2 <valencialeonardo905@example.com>
2025-05-13 09:52:45 -0500

    Se implemento un retraso de 5 segundos por cada verificación de login

--- vistas/scripts/login.js
+++ vistas/scripts/login.js
@@ -1,6 +1,17 @@
 document.getElementById("frmAcceso").addEventListener("submit", function(e) {
     e.preventDefault();
 
+        // Seleccionar el botón por su clase
+    const submitButton = document.querySelector(".btn.btn-primary.btn-block.btn-flat"); 
+
+    // Deshabilitar el botón de enviar
+    submitButton.disabled = true;
+
+    // Añadir un retraso de 5 segundos
+    setTimeout(function() {
+        submitButton.disabled = false; // Habilitar nuevamente el botón después de 5 segundos
+    }, 5000);
+    
     const logina = document.getElementById("logina").value.trim();
     const clavea = document.getElementById("clavea").value.trim();
 
